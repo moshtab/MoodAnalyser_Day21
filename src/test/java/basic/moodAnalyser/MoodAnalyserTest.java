d41ee71884f73e8118af1635d3df6d302ea2b8cc
moshtab <mdmohsin41439@gmail.com>
2021-08-19 23:25:29 +0530

    Added default Constructor and Parameterised Constructor

--- src/test/java/basic/moodAnalyser/MoodAnalyserTest.java
+++ src/test/java/basic/moodAnalyser/MoodAnalyserTest.java
@@ -2,20 +2,26 @@ package basic.moodAnalyser;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
-	MoodAnalyser mood;
 
-	@Before
-	public void initialize() {
-		mood = new MoodAnalyser();
+	@Test  //using default Constructor
+	public void shouldReturnHappyOrSadByRespondingFromAMessageUsingConstructor() {
+		MoodAnalyser mood = new MoodAnalyser();
+		assertEquals("SAD", mood.analyseMood());
 	}
 
-	@Test
+	@Test //using parameterised Constructor
+	public void shouldReturnHappyOrSadByRespondingFromAMessageUsingConstructorParameter() {
+		MoodAnalyser mood = new MoodAnalyser("Iam Feeling happy ");
+		assertEquals("HAPPY", mood.analyseMood());
+	}
+
+	@Test //normal method
 	public void shouldReturnHappyOrSadByRespondingFromAMessage() {
+		MoodAnalyser mood = new MoodAnalyser();
 		assertEquals("HAPPY", mood.analyseMood("Iam in any Mood "));
 	}
-	
+
 }
